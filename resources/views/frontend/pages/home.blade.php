1f93cd1bf21894275029b4d947ed2f43f70b3463
MohanManirul <fiforeg@gmail.com>
2024-11-13 16:29:23 +0600

    some changes

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -22,6 +22,7 @@
             <div class="col-xl-7 ps-0 ps-lg-5 pe-lg-1 d-flex align-items-stretch">
               <div class="content d-flex flex-column justify-content-center">
                 <h3>About shifkat</h3>
+                <h3>About mohan</h3>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
                 </p>
